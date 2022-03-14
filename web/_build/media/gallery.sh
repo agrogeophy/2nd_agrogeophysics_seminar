d5a26eca7a2922aaf7579022520668a3f73c43ee
BenjMy <36472081+BenjMy@users.noreply.github.com>
2022-03-14 11:30:22 -0700

    gallery (Ellen pics)

---
+++ web/_build/media/gallery.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in *.jpg 
+do 
+      
+echo  " <div class='row'> <div class='col-lg-4 col-md-12 mb-4 mb-lg-0'> <img src='media/$i'     class='w-100 shadow-1-strong rounded mb-4' alt='© E.VanDeVijver'/> </div> </div>" >> all.html
+
+done
+	
+
---
+++ web/_build/media/selection/gallery.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in *.JPG 
+do 
+      
+echo "<div class='gallery'><a target='_blank' href='media/selection/$i'><img src='media/selection/$i' alt='© E.VanDeVijver' width='600' height='400'></a></div>" >> all.html
+
+
+done
+	
+
